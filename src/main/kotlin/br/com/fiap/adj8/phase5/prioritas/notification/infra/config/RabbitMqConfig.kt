de7d949836a33f3c2515577d52844f67e3d299f4
Gabriel Soares <gabrielalcantarars@gmail.com>
2026-02-07 16:01:49 -0300

    feat: Fixing sync between services

--- src/main/kotlin/br/com/fiap/adj8/phase5/prioritas/notification/infra/config/RabbitMqConfig.kt
+++ src/main/kotlin/br/com/fiap/adj8/phase5/prioritas/notification/infra/config/RabbitMqConfig.kt
@@ -1,9 +1,11 @@
 package br.com.fiap.adj8.phase5.prioritas.notification.infra.config
 
+import br.com.fiap.adj8.phase5.prioritas.common.event.TRIAGE_QUEUE
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.registerKotlinModule // <--- IMPORTANTE: Importe isso
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import org.springframework.amqp.core.Queue
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
 import org.springframework.amqp.support.converter.MessageConverter
 import org.springframework.context.annotation.Bean
@@ -18,5 +20,10 @@ class RabbitMqConfig {
         mapper.registerKotlinModule()
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
         return Jackson2JsonMessageConverter(mapper)
+    }
+
+    @Bean
+    fun triageQueue(): Queue {
+        return Queue(TRIAGE_QUEUE, true)
     }
 }
